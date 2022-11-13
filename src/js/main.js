3568655b044e8bc694f301cfc3a68d6aa29b9abf
Mauricio <jourdanmauricio@gmail.com>
2022-11-13 15:25:27 -0300

    First Commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
